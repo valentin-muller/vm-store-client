659637f49772c2e9733cd12e0d8584de4237f978
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-10-30 12:07:14 +0100

    Cart items total in menu

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,50 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ShowImage from './showImage'
-import moment from 'moment'
-
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import ShowImage from "./showImage";
+import moment from "moment";
+import { addItem } from "./cartHelpers";
 
 const Card = ({ product, showViewProductButton = true }) => {
-
+  const [redirect, setRedirect] = useState(false);
 
   const showViewButton = (showViewProductButton) => {
     return (
       showViewProductButton && (
         <Link to={`/product/${product._id}`}>
-          <button>
-            View Product
-          </button>
+          <button>View Product</button>
         </Link>
       )
-      )
-  }
+    );
+  };
+
+  const addToCart = () => {
+    addItem(product, () => {
+      setRedirect(true);
+    });
+  };
+
+  const shouldRedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
 
   const showAddToCartButton = () => {
-    return (
-              <button>Add to card</button>
-
-    )
-  }
+    return <button onClick={addToCart}>Add to card</button>;
+  };
 
   const showStock = (quantity) => {
-    return quantity > 0 ? <span>In Stock</span> : <span>Out of Stock</span>
-  }
+    return quantity > 0 ? <span>In Stock</span> : <span>Out of Stock</span>;
+  };
 
   return (
     <div>
       <div>
         <div>{product.name}</div>
-        <ShowImage item={ product } url="product" />
+        {shouldRedirect(redirect)}
+        <ShowImage item={product} url="product" />
         <p>{product.description.substring(0, 100)}</p>
         <p>{product.price}</p>
         <p>Category: {product.category && product.category.name}</p>
-        <p>
-          Added on {moment(product.createdAt).fromNow()}
-        </p>
-          {showStock(product.quantity)}
-          <br />
-          {showViewButton(showViewProductButton)}
-          {showAddToCartButton()}
+        <p>Added on {moment(product.createdAt).fromNow()}</p>
+        {showStock(product.quantity)}
+        <br />
+        {showViewButton(showViewProductButton)}
+        {showAddToCartButton()}
       </div>
     </div>
   );
--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { signout, isAuthenticated } from "../auth";
+import {itemTotal} from './cartHelpers'
 
 const isActive = (history, path) => {
   if (history.location.pathname === path) {
@@ -23,6 +24,12 @@ const Menu = ({ history }) => {
         <li>
           <Link to="/shop" style={isActive(history, "/shop")}>
             Shop
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/cart" style={isActive(history, "/cart")}>
+            Cart <sup><small>{itemTotal()}</small></sup>
           </Link>
         </li>
 
